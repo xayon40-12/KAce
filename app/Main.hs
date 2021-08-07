e155b85f98e4de943a95c01d5c17638c32e94abe
Nathan Touroux <touroux.nathan@gmail.com>
2021-08-07 13:01:54 +0200

    optimize

--- app/Main.hs
+++ app/Main.hs
@@ -12,4 +12,4 @@ main :: IO ()
 main = do
   g <- getStdGen
   let ran0 = randoms g
-  play window white 300 (initWorld ran0) draw input update
+  play window white 60 (initWorld ran0) draw input update
--- src/World.hs
+++ src/World.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE UnboxedTuples #-}
 
 module World where
 
 import Ball
 import Brick
 import Constants
+-- import Data.List.Stream
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import Lens.Micro
@@ -35,7 +37,7 @@ topleft :: [Picture] -> Picture
 topleft = Translate (- sw / 2) (sh / 2) . Pictures
 
 initWorld :: Ran -> World
-initWorld ran0 = World 1 0 ran1 ([], 1) nbricks (Aiming out)
+initWorld ran0 = World 1 0 ran1 ([], 1000) nbricks (Aiming out)
   where
     (ran1, nbricks) = newBricks 1 ran0
 
@@ -55,44 +57,54 @@ input (EventMotion (x, y)) w | isAiming (w ^. status) = w & status .~ Aiming (x,
 input (EventKey (MouseButton LeftButton) Down _ (x, y)) w | isAiming (w ^. status) && y > bh - sh / 2 = w & status .~ Playing & balls . _1 .~ nballs
   where
     nballs = [Ball (px - i * d * dx, py - i * d * dy) (dx, dy) | i <- fromIntegral <$> [0 .. w ^. balls . _2 -1]]
-    d = 4 * ballSize
+    d = 4 * ballSize * 0.1
     (dx, dy) = normalize (x, y - sh / 2 - py)
     normalize (a, b) = let l = sqrt (a * a + b * b) in (a / l, b / l)
 input _ w = w
 
 update :: Float -> World -> World
-update _ w
+update t = go v
+  where
+    v = 20 :: Int
+    go 0 w = w
+    go i w = case update' t w of
+      w' | isAiming (w' ^. status) -> w'
+      w' -> go (i -1) w'
+
+update' :: Float -> World -> World
+update' _ w
   | null (w ^. balls . _1) && isPlaying (w ^. status) =
     if reachedBottom (w ^. bricks)
       then w & status .~ Dead
-      else let (nran, nbricks) = newRow n (w ^. ran) (w ^. bricks) in w & bricks .~ nbricks & status .~ Aiming out & stage .~ n & ran .~ nran
-  where
-    n = w ^. stage + 1
-update _ w = case w ^. status of
+      else
+        let (nran, nbricks) = newRow n (w ^. ran) (w ^. bricks)
+            n = w ^. stage + 1
+         in w & bricks .~ nbricks & status .~ Aiming out & stage .~ n & ran .~ nran
+update' _ w = case w ^. status of
   Playing -> w & balls . _1 .~ nballs & bricks .~ nbricks & balls . _2 +~ count & score +~ dels
     where
       balls' = filter insidewalls $ clampwalls . move <$> w ^. balls . _1
       bricks' = w ^. bricks
       (nballs, dels, (nbricks, count)) = if null balls' then (balls', 0, (bricks', 0)) else foldl foldballs ([], 0, (bricks', 0)) balls'
       foldballs (bs, del, (brs, c)) b = let (nb, del', nbrs) = foldl foldbricks (b, del, ([], c)) brs in (nb : bs, del', nbrs)
-      foldbricks (b, del, brs) br = let (nb, nbr, del') = collide b br in (nb, del + del', addbrick nbr brs)
+      foldbricks (b, del, brs) br = let (# nb, nbr, del' #) = collide b br in (nb, del + del', addbrick nbr brs)
       addbrick br (brs, c) = if alive br then (br : brs, c) else (brs, c + 1)
   _ -> w
 
-collide :: Num c => Ball -> Brick -> (Ball, Brick, c)
+collide :: Num c => Ball -> Brick -> (# Ball, Brick, c #)
 collide b@(Ball (x, y) (dx, dy)) br =
   if inside b br
     then
       if ur
         then
           if dr
-            then (Ball (x, y) (abs dx, dy), br & life -~ 1, 1)
-            else (Ball (x, y) (dx, abs dy), br & life -~ 1, 1)
+            then let dx' = abs dx in (# Ball (x + dx', y) (dx', dy), br & life -~ 1, 1 #)
+            else let dy' = abs dy in (# Ball (x, y + dy') (dx, dy'), br & life -~ 1, 1 #)
         else
           if dr
-            then (Ball (x, y) (dx, - abs dy), br & life -~ 1, 1)
-            else (Ball (x, y) (- abs dx, dy), br & life -~ 1, 1)
-    else (b, br, 0)
+            then let dy' = - abs dy in (# Ball (x, y + dy') (dx, dy'), br & life -~ 1, 1 #)
+            else let dx' = - abs dx in (# Ball (x + dx', y) (dx', dy), br & life -~ 1, 1 #)
+    else (# b, br, 0 #)
   where
     (bx, by) = br ^. pos
     ur = - bh * (x - bx) - bw * (y - by) < 0
